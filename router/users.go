5fde440ef459747247be1b05d0a8eb8b994d51a4
anko9801 <daiusa3@icloud.com>
2022-12-15 01:30:38 +0900

    :art: add user router

--- router/users.go
+++ router/users.go
@@ -1,15 +1,30 @@
 package router
 
 import (
+	"net/http"
+
+	"github.com/Hackathon22-Winter-03/backend/model"
 	"github.com/labstack/echo/v4"
 )
 
 // GET /users
 func getUsersHandler(c echo.Context) error {
-	return nil
+	users, err := model.GetUsers(c.Request().Context())
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, users)
 }
 
 // GET /users/:userID
 func getUserHandler(c echo.Context) error {
-	return nil
+	userID := c.Param("userID")
+
+	user, err := model.GetUser(c.Request().Context(), userID)
+
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, user)
 }
