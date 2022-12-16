fbc25d7f06c0e1a55a723a060957bc2b2b07176b
anko9801 <daiusa3@icloud.com>
2022-12-16 12:23:13 +0900

    wakarazu

--- model/problems.go
+++ model/problems.go
@@ -2,20 +2,21 @@ package model
 
 import (
 	"context"
+	"database/sql"
 	"time"
 
 	"github.com/Hackathon22-Winter-03/backend/utils"
 )
 
 type Problem struct {
-	ID        string     `json:"id" db:"id"`
-	CreatorID string     `json:"creatorId" db:"creator_id"`
-	Score     int        `json:"score" db:"score"`
-	Title     string     `json:"title" db:"title"`
-	Text      string     `json:"text" db:"text"`
-	CreatedAt time.Time  `json:"createdAt" db:"created_at"`
-	UpdatedAt time.Time  `json:"updatedAt" db:"updated_at"`
-	DeletedAt *time.Time `json:"deletedAt" db:"deleted_at"`
+	ID        string       `json:"id" db:"id"`
+	CreatorID string       `json:"creatorId" db:"creator_id"`
+	Score     int          `json:"score" db:"score"`
+	Title     string       `json:"title" db:"title"`
+	Text      string       `json:"text" db:"text"`
+	CreatedAt time.Time    `json:"createdAt" db:"created_at"`
+	UpdatedAt time.Time    `json:"updatedAt" db:"updated_at"`
+	DeletedAt sql.NullTime `json:"deletedAt" db:"deleted_at"`
 }
 
 func GetProblems(ctx context.Context) ([]Problem, error) {
