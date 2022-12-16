041604dcbb78555855cdbe005c443e0df256a2c7
anko9801 <daiusa3@icloud.com>
2022-12-16 22:00:13 +0900

    :art: arrage type of columns

--- model/problems.go
+++ model/problems.go
@@ -2,33 +2,32 @@ package model
 
 import (
 	"context"
-	"database/sql"
 	"time"
 
 	"github.com/Hackathon22-Winter-03/backend/utils"
 )
 
 type Problem struct {
-	ID        string       `json:"id" db:"id"`
-	CreatorID string       `json:"creatorId" db:"creator_id"`
-	Score     int          `json:"score" db:"score"`
-	Title     string       `json:"title" db:"title"`
-	Text      string       `json:"text" db:"text"`
-	CreatedAt time.Time    `json:"createdAt" db:"created_at"`
-	UpdatedAt time.Time    `json:"updatedAt" db:"updated_at"`
-	DeletedAt sql.NullTime `json:"deletedAt" db:"deleted_at"`
+	ID        string     `json:"id" db:"id"`
+	CreatorID string     `json:"creatorId" db:"creator_id"`
+	Score     int        `json:"score" db:"score"`
+	Title     string     `json:"title" db:"title"`
+	Text      string     `json:"text" db:"text"`
+	CreatedAt time.Time  `json:"createdAt" db:"created_at"`
+	UpdatedAt time.Time  `json:"updatedAt" db:"updated_at"`
+	DeletedAt *time.Time `json:"deletedAt" db:"deleted_at"`
 }
 
 type ProblemAggregate struct {
-	ID          string       `json:"id" db:"id"`
-	CreatorID   string       `json:"creatorId" db:"creator_id"`
-	CreatorName string       `json:"creatorName" db:"creator_name"`
-	Score       int          `json:"score" db:"score"`
-	Title       string       `json:"title" db:"title"`
-	Text        string       `json:"text" db:"text"`
-	CreatedAt   time.Time    `json:"createdAt" db:"created_at"`
-	UpdatedAt   time.Time    `json:"updatedAt" db:"updated_at"`
-	DeletedAt   sql.NullTime `json:"deletedAt" db:"deleted_at"`
+	ID          string     `json:"id" db:"id"`
+	CreatorID   string     `json:"creatorId" db:"creator_id"`
+	CreatorName string     `json:"creatorName" db:"creator_name"`
+	Score       int        `json:"score" db:"score"`
+	Title       string     `json:"title" db:"title"`
+	Text        string     `json:"text" db:"text"`
+	CreatedAt   time.Time  `json:"createdAt" db:"created_at"`
+	UpdatedAt   time.Time  `json:"updatedAt" db:"updated_at"`
+	DeletedAt   *time.Time `json:"deletedAt" db:"deleted_at"`
 }
 
 func GetProblems(ctx context.Context) ([]Problem, error) {
