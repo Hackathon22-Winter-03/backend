2c076310fbc39b4e82d07d9f7801ad489920d746
anko9801 <daiusa3@icloud.com>
2022-12-16 20:09:56 +0900

    :bug: 伝家の宝刀: にぶたん
    for error

--- model/problems.go
+++ model/problems.go
@@ -23,7 +23,7 @@ func GetProblems(ctx context.Context) ([]Problem, error) {
 	problems := []Problem{}
 	err := dbx.Select(
 		&problems,
-		"SELECT `id`, `creator_id`, `score`, `title`, `created_at`, `updated_at`, `deleted_at` "+
+		"SELECT `id`, `creator_id`, `score`, `title` "+
 			"FROM problems",
 	)
 	if err != nil {
