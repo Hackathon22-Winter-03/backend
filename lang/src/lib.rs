a60dafeb67496c129ff6705cebe2ee3d1234b25e
anko9801 <daiusa3@icloud.com>
2022-12-18 12:00:15 +0900

    onegai

--- lang/src/lib.rs
+++ lang/src/lib.rs
@@ -6,7 +6,10 @@ use std::ffi::{CStr, CString};
 use crate::markov::*;
 
 #[no_mangle]
-pub extern "C" fn simulate(code: *const libc::c_char, input: *const libc::c_char) -> *const libc::c_char {
+pub extern "C" fn simulate(
+    code: *const libc::c_char,
+    input: *const libc::c_char,
+) -> *const libc::c_char {
     let cstr_code = unsafe { CStr::from_ptr(code) };
     let cstr_input = unsafe { CStr::from_ptr(input) };
     let str_code = cstr_code.to_str().unwrap();
@@ -17,16 +20,20 @@ pub extern "C" fn simulate(code: *const libc::c_char, input: *const libc::c_char
         Ok(mut markov) => {
             markov.set_text(str_input);
             str_output = markov.run();
-            CString::new(str_output.into_iter().collect::<String>()).unwrap().into_raw()
-        },
-        Err(msg) => {
-            CString::new(msg.to_string()).unwrap().into_raw()
-        },
+            CString::new(str_output.into_iter().collect::<String>())
+                .unwrap()
+                .into_raw()
+        }
+        Err(msg) => CString::new(msg.to_string()).unwrap().into_raw(),
     }
 }
 
 #[no_mangle]
-pub extern "C" fn step_execute(code: *const libc::c_char, input: *const libc::c_char, model: *const libc::c_char) -> *const libc::c_char {
+pub extern "C" fn step_execute(
+    code: *const libc::c_char,
+    input: *const libc::c_char,
+    model: *const libc::c_char,
+) -> *const libc::c_char {
     let cstr_code = unsafe { CStr::from_ptr(code) };
     let cstr_input = unsafe { CStr::from_ptr(input) };
     let cstr_model = unsafe { CStr::from_ptr(model) };
@@ -36,16 +43,23 @@ pub extern "C" fn step_execute(code: *const libc::c_char, input: *const libc::c_
     let str_model = cstr_model.to_str().unwrap();
 
     let str_output;
+    println!("{} {} {}", str_code, str_input, str_model);
     if str_model == "markov" {
+        println!("markov desu");
         match Markov::new(str_code) {
             Ok(mut markov) => {
+                println!("ok");
                 markov.set_text(str_input);
                 str_output = markov.step().0;
-                return CString::new(str_output.into_iter().collect::<String>()).unwrap().into_raw();
-            },
+                println!("{:?}", str_output);
+                return CString::new(str_output.into_iter().collect::<String>())
+                    .unwrap()
+                    .into_raw();
+            }
             Err(msg) => {
+                println!("err");
                 return CString::new(msg.to_string()).unwrap().into_raw();
-            },
+            }
         }
     }
 
@@ -53,8 +67,8 @@ pub extern "C" fn step_execute(code: *const libc::c_char, input: *const libc::c_
 }
 
 fn main() {
-//     if let Ok(mut markov) = Markov::new("woman:W\nman:M\nMW:\nWM:\n") {
-//         markov.set_text("manmanwomanwomanmanwomanwomanmanwomanmanmanwoman");
-//         println!("{:?}", markov.run());
-//     }
+    //     if let Ok(mut markov) = Markov::new("woman:W\nman:M\nMW:\nWM:\n") {
+    //         markov.set_text("manmanwomanwomanmanwomanwomanmanwomanmanmanwoman");
+    //         println!("{:?}", markov.run());
+    //     }
 }
